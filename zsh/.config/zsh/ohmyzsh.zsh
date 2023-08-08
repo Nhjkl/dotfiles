ba4f1437b7d823a28243a7cc62baa6fac4182d5c
liuxian <liuxiangogo@gmail.com>
2023-08-09 01:23:51 +0800

    update

--- zsh/.config/zsh/ohmyzsh.zsh
+++ zsh/.config/zsh/ohmyzsh.zsh
@@ -6,17 +6,15 @@ fi
 
 ZSHPLUGIN=$ZSH/custom/plugins
 
-cd $ZSHPLUGIN;
 if [ ! -d $ZSHPLUGIN/fast-syntax-highlighting ] ; then
-  git clone --depth=1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git;
+  git clone --depth=1 https://github.com/zdharma-continuum/fast-syntax-highlighting.git $ZSHPLUGIN/fast-syntax-highlighting;
 fi
 if [ ! -d $ZSHPLUGIN/zsh-autosuggestions ] ; then
-  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git;
+  git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git $ZSHPLUGIN/zsh-autosuggestions;
 fi
 if [ ! -d $ZSHPLUGIN/fzf-tab ] ; then
-  git clone --depth=1 https://github.com/Aloxaf/fzf-tab.git;
+  git clone --depth=1 https://github.com/Aloxaf/fzf-tab.git $ZSHPLUGIN/fzf-tab;
 fi
-cd $HOME;
 
 # Load syntax highlighting; should be last.
 # source $ZSHPLUGIN/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh 2>/dev/null
