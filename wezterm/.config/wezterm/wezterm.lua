d2b5545698a409921cf663012fd21d37f5a21f96
liuxian <liuxiangogo@gmail.com>
2024-03-29 20:35:34 +0800

    update

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,12 +4,14 @@ local config = wezterm.config_builder()
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.8
 config.color_scheme = "tokyonight_moon"
+-- config.color_scheme = "Gruvbox Material (Gogh)"
 config.enable_tab_bar = false
+config.audible_bell = "Disabled"
 config.font_size = 16.0
 
 config.window_padding = {
-	left = 6,
-	right = 6,
+	left = 0,
+	right = 0,
 	top = 0,
 	bottom = 0,
 }
